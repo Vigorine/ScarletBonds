93fac6e19d7e2438296c15c642763981deacbab6
Vigorine <crypt@MSI.Home>
2024-07-21 19:53:36 +0100

    Added TestProcedure2

---
+++ src/main/java/net/scarletbonds/procedures/TestProcedure2Procedure.java
@@ -0,0 +1,10 @@
+package net.scarletbonds.procedures;
+
+import java.util.Map;
+
+public class TestProcedure2Procedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+
+	}
+}
