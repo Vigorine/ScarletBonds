b2aefe4c70d3aa0d9f20e91ec35f098e3e12eecb
Vigorine <crypt@MSI.Home>
2024-07-21 19:47:56 +0100

    Test

---
+++ src/main/java/net/scarletbonds/procedures/TestProcedureProcedure.java
@@ -0,0 +1,10 @@
+package net.scarletbonds.procedures;
+
+import java.util.Map;
+
+public class TestProcedureProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+
+	}
+}
